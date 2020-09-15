858288145239f8cb204625a052e2f9a8261507d8
uko010199@gmail.com <uko010199@gmail.com>
2020-09-15 10:16:28 +0900

    revise VO

--- src/Bank/VO/BankVO.java
+++ src/Bank/VO/BankVO.java
@@ -2,4 +2,64 @@ package Bank.VO;
 
 public class BankVO {
 
+	private String name;
+	private String nameOfBank;
+	private String accountNumber;
+	private String gender;
+	private String tel;
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getNameOfBank() {
+		return nameOfBank;
+	}
+
+	public void setNameOfBank(String nameOfBank) {
+		this.nameOfBank = nameOfBank;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "은행명: " + nameOfBank + "\n계좌번호" + accountNumber + "\n이름: " + name + "\n비밀번호: " + password + "\n성별: " + gender + "\n전화번호: " + tel;
+	}
+
 }
