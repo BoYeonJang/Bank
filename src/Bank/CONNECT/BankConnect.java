07e10f19b635bf04643683498625e136dd0e85ee
uko010199@gmail.com <uko010199@gmail.com>
2020-09-17 09:29:41 +0900

    modify Connect

--- src/Bank/CONNECT/BankConnect.java
+++ src/Bank/CONNECT/BankConnect.java
@@ -2,6 +2,9 @@ package Bank.CONNECT;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class BankConnect {
 
@@ -19,5 +22,49 @@ public class BankConnect {
 		Class.forName(DRIVER_NAME);
 		Connection conn = DriverManager.getConnection(url, user, password);
 		return conn;
+	}
+
+	// close
+	public static void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(Connection conn, PreparedStatement ps) {
+		if (ps != null) {
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		close(conn);
+	}
+
+	public static void close(Connection conn, PreparedStatement ps, ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		close(conn, ps);
+	}
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		try {
+			conn = BankConnect.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(conn);
 	}
 }
